eab699c6116cf1ef1672239bc5973c38f26026dc
jonathans199 <jso1919@gmail.com>
2023-02-23 18:35:35 -0500

    first commit set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log(process.env.REACT_APP_API_ENDPOINT)
+
+	useEffect(() => {
+		fetch(process.env.REACT_APP_API_ENDPOINT)
+			.then(res => res.json())
+			.then(data => console.log('data from server ->', data))
+			.catch(err => console.error(err))
+	}, [])
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> hey class.
+				</p>
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
